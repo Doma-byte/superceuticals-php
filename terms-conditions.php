85d303095236013b758cc9574fbebe44dbb29925
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:50:14 +0530

    Add t&c page

---
+++ terms-conditions.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superceuticals Engineering Healthcare</title>
+    <?php include 'headlink.php';?>
+</head>
+<body style="font-family: Arial,sans-serif; line-height: 1.6; margin: 0; padding:0;">
+    <!-- Header Area Start-->
+    <?php include 'header.php';?>
+    <!-- Header Area End -->
+    <div class="container" style="max-width: 800px; margin: 0 auto; padding: 20px;">
+        <h1 style="font-size: 26px; margin: 20px;">Superceuticals Private Limited</h1>
+        <h2 style="font-size: 24px; margin-bottom: 10px;">Terms & Conditions</h2>
+        <p style="margin-bottom: 10px;">Last updated on Jun 21st 2023</p>
+
+        <p style="margin-bottom: 10px;">The Website Owner, including subsidiaries and affiliates ("Website" or "Website Owner" or "we" or "us" or "our") provides the information contained on the website or any of the pages comprising the website ("website") to visitors ("visitors") (cumulatively referred to as "you" or "your" hereinafter) subject to the terms and conditions set out in these website terms and conditions, the privacy policy and any other relevant terms and conditions, policies and notices which may be applicable to a specific section or module of the website.</p>
+
+        <p style="margin-bottom: 10px;">Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern Superceuticals Private Limited's relationship with you in relation to this website.</p>
+
+        <p style="margin-bottom: 10px;">The term 'Superceuticals Private Limited' or 'us' or 'we' refers to the owner of the website whose registered/operational office is No. 1081/17, Kheri Kalan Faridabad HARYANA 121002. The term 'you' refers to the user or viewer of our website.</p>
+
+        <p style="margin-bottom: 10px;">The use of this website is subject to the following terms of use:</p>
+
+        <ul style="list-style-type: disc; margin-bottom: 10px; padding-left: 20px;">
+            <li>The content of the pages of this website is for your general information and use only. It is subject to change without notice.</li>
+            <li>Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness, or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors, and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.</li>
+            <li>Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services, or information available through this website meet your specific requirements.</li>
+            <li>This website contains material that is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance, and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.</li>
+            <li>All trademarks reproduced on this website that are not the property of, or licensed to, the operator are acknowledged on the website.</li>
+            <li>Unauthorized use of this website may give rise to a claim for damages and/or be a criminal offense.</li>
+            <li>From time to time, this website may also include links to other websites. These links are provided for your convenience to provide further information.</li>
+            <li>You may not create a link to this website from another website or document without Superceuticals Private Limited's prior written consent.</li>
+            <li>Your use of this website and any dispute arising out of such use of the website is subject to the laws of India or other regulatory authority.</li>
+            <li>We, as a merchant, shall be under no liability whatsoever in respect of any loss or damage arising directly or indirectly out of the decline of authorization for any Transaction, on Account of the Cardholder having exceeded the preset limit mutually agreed by us with our acquiring bank from time to time.</li>
+        </ul>
+    </div>
+    <!-- Footer Area Start-->
+    <?php include 'footer.php';?>
+    <!-- Footer Area End-->
+</body>
+</html>
