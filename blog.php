e02378c5168c423f07e213f93dec83a92b9edaff
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:47:46 +0530

    Add blog page

---
+++ blog.php
@@ -0,0 +1,57 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Blog | Superceuticals Engineering Healthcare</title>
+		<?php include 'headlink.php';?>
+	</head>
+	<body>
+		<!-- Header Area Start-->
+		<?php include 'header.php';?>
+		<!-- Header Area End -->
+		<section class="InnerBanner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<h1>Blog</h1>
+						<nav class="breadcrumb">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+								<li class="breadcrumb-item active" aria-current="page">Blog</li>
+							</ol>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+		<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1>Comming Soon</h1>
+            </div>
+        </div>
+    </div>
+</section>
+		
+		
+
+
+
+
+
+
+		<!-- Footer Area Start-->
+		<?php include 'footer.php';?>
+		<!-- Footer Area End-->
+		
+	</body>
+</html>
+
