12c3919299e10fcf7cd5ff1e8673c88aef97c806
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:50:01 +0530

    Add team page

---
+++ team.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Team | Superceuticals Engineering Healthcare</title>
+    <?php include 'headlink.php';?>
+</head>
+
+<body>
+<!-- Header Area Start-->
+<?php include 'header.php';?>
+<!-- Header Area End -->
+
+
+<section class="InnerBanner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h1>Our Team</h1>
+            <nav class="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Our Team</li>
+                        </ol>
+                    </nav>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Team Page Section -->
+<section class="teamWrapper">
+		<div class="container">			
+			<div class="row clearfix">
+				<div class="team-block-two style-two col-lg-4 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeIn" data-wow-delay="0ms" data-wow-duration="1500ms">
+						<div class="image">
+							<a href="#"><img src="src/img/team/team1.jpg" alt="" title=""></a>
+							<!-- <ul class="social-box">
+								<li><a href="#" class="fa fa-facebook"></a></li>
+								<li><a href="#" class="fa fa-twitter"></a></li>
+								<li><a href="#" class="fa fa-linkedin"></a></li>
+								<li><a href="#" class="fa fa-google-plus"></a></li>
+							</ul> -->
+						</div>
+						<div class="lower-content">
+							<h5><a href="#">Frazer Diamond</a></h5>
+							<div class="designation">Founder & CEO</div>
+						</div>
+					</div>
+				</div>
+				<div class="team-block-two style-two col-lg-4 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeIn" data-wow-delay="0ms" data-wow-duration="1500ms">
+						<div class="image">
+                        <a href="#"><img src="src/img/team/team2.jpg" alt="" title=""></a>
+							<!-- <ul class="social-box">
+								<li><a href="#" class="fa fa-facebook"></a></li>
+								<li><a href="#" class="fa fa-twitter"></a></li>
+								<li><a href="#" class="fa fa-linkedin"></a></li>
+								<li><a href="#" class="fa fa-google-plus"></a></li>
+							</ul> -->
+						</div>
+						<div class="lower-content">
+							<h5><a href="#">Chen Lee</a></h5>
+							<div class="designation">Co-Founder</div>
+						</div>
+					</div>
+				</div>
+				<div class="team-block-two style-two col-lg-4 col-md-12 col-sm-12">
+					<div class="inner-box wow fadeIn" data-wow-delay="0ms" data-wow-duration="1500ms">
+						<div class="image">
+                        <a href="#"><img src="src/img/team/team3.jpg" alt="" title=""></a>
+							<!-- <ul class="social-box">
+								<li><a href="#" class="fa fa-facebook"></a></li>
+								<li><a href="#" class="fa fa-twitter"></a></li>
+								<li><a href="#" class="fa fa-linkedin"></a></li>
+								<li><a href="#" class="fa fa-google-plus"></a></li>
+							</ul> -->
+						</div>
+						<div class="lower-content">
+							<h5><a href="#">Emma O’Brien</a></h5>
+							<div class="designation">Sr. Product Engineer</div>
+						</div>
+					</div>
+				</div>
+				<div class="team-block-two style-two col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeIn" data-wow-delay="0ms" data-wow-duration="1500ms">
+						<div class="image">
+                        <a href="#"><img src="src/img/team/team4.jpg" alt="" title=""></a>
+							<!-- <ul class="social-box">
+								<li><a href="#" class="fa fa-facebook"></a></li>
+								<li><a href="#" class="fa fa-twitter"></a></li>
+								<li><a href="#" class="fa fa-linkedin"></a></li>
+								<li><a href="#" class="fa fa-google-plus"></a></li>
+							</ul> -->
+						</div>
+						<div class="lower-content">
+							<h5><a href="#">David Ankrum</a></h5>
+							<div class="designation">Co Founder</div>
+						</div>
+					</div>
+				</div>
+				<div class="team-block-two style-two col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeIn" data-wow-delay="0ms" data-wow-duration="1500ms">
+						<div class="image">
+                        <a href="#"><img src="src/img/team/team5.jpg" alt="" title=""></a>
+							<!-- <ul class="social-box">
+								<li><a href="#" class="fa fa-facebook"></a></li>
+								<li><a href="#" class="fa fa-twitter"></a></li>
+								<li><a href="#" class="fa fa-linkedin"></a></li>
+								<li><a href="#" class="fa fa-google-plus"></a></li>
+							</ul> -->
+						</div>
+						<div class="lower-content">
+							<h5><a href="#">Maria De Aragon</a></h5>
+							<div class="designation">HRM</div>
+						</div>
+					</div>
+				</div>
+				<div class="team-block-two style-two col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeIn" data-wow-delay="0ms" data-wow-duration="1500ms">
+						<div class="image">
+                        <a href="#"><img src="src/img/team/team6.jpg" alt="" title=""></a>
+							<!-- <ul class="social-box">
+								<li><a href="#" class="fa fa-facebook"></a></li>
+								<li><a href="#" class="fa fa-twitter"></a></li>
+								<li><a href="#" class="fa fa-linkedin"></a></li>
+								<li><a href="#" class="fa fa-google-plus"></a></li>
+							</ul> -->
+						</div>
+						<div class="lower-content">
+							<h5><a href="#">Joe Hurt</a></h5>
+							<div class="designation">CTO</div>
+						</div>
+					</div>
+				</div>
+				<div class="team-block-two style-two col-lg-3 col-md-6 col-sm-12">
+					<div class="inner-box wow fadeIn" data-wow-delay="0ms" data-wow-duration="1500ms">
+						<div class="image">
+                        <a href="#"><img src="src/img/team/team7.jpg" alt="" title=""></a>
+							<!-- <ul class="social-box">
+								<li><a href="#" class="fa fa-facebook"></a></li>
+								<li><a href="#" class="fa fa-twitter"></a></li>
+								<li><a href="#" class="fa fa-linkedin"></a></li>
+								<li><a href="#" class="fa fa-google-plus"></a></li>
+							</ul> -->
+						</div>
+						<div class="lower-content">
+							<h5><a href="#">Damien Wayne</a></h5>
+							<div class="designation">COO</div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			
+		</div>
+	</section>
+	<!-- End Team Page Section -->
+
+
+
+
+
+<!-- Footer Area Start-->
+<?php include 'footer.php';?>
+<!-- Footer Area End-->
+</body>
+
+</html>
