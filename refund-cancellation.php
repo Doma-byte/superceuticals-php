5fbed7e70b6977932f0d6b61497f74746352e6c2
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:49:50 +0530

    Add refund-cancellation page

---
+++ refund-cancellation.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superceuticals Engineering Healthcare</title>
+    <?php include 'headlink.php';?>
+</head>
+<body style="font-family: Arial,sans-serif; line-height: 1.6; margin: 0; padding:0;">
+    <!-- Header Area Start-->
+    <?php include 'header.php';?>
+    <!-- Header Area End -->
+    <div class="container" style="max-width: 800px; margin: 0 auto; padding: 20px;">
+        <h1 style="font-size: 26px; margin: 20px;">Superceuticals Private Limited</h1>
+        <h2 style="font-size: 24px; margin-bottom: 10px;">Cancellation & Refund Policy</h2>
+        <p style="margin-bottom: 10px;">Last updated on Jun 20th 2023</p>
+
+        <p style="margin-bottom: 10px;">Superceuticals Private Limited believes in helping its customers as far as possible and has, therefore, a liberal cancellation policy. Under this policy:</p>
+
+        <ul style="list-style-type: disc; margin-bottom: 10px; padding-left: 20px;">
+            <li>Cancellations will be considered only if the request is made immediately after placing the order. However, the cancellation request may not be entertained if the orders have been communicated to the vendors/merchants and they have initiated the process of shipping them.</li>
+            <li>Superceuticals Private Limited does not accept cancellation requests for perishable items like flowers, eatables, etc. However, a refund/replacement can be made if the customer establishes that the quality of the product delivered is not good.</li>
+            <li>In case of receipt of damaged or defective items, please report the same to our Customer Service team. The request will, however, be entertained once the merchant has checked and determined the same at his own end. This should be reported within 7 days of receipt of the products.</li>
+            <li>In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within 7 days of receiving the product. The Customer Service Team, after looking into your complaint, will take an appropriate decision.</li>
+            <li>In case of complaints regarding products that come with a warranty from manufacturers, please refer the issue to them.</li>
+            <li>In case of any refunds approved by Superceuticals Private Limited, it will take 1-2 days for the refund to be processed to the end customer.</li>
+        </ul>
+    </div>
+    <!-- Footer Area Start-->
+    <?php include 'footer.php';?>
+    <!-- Footer Area End-->
+</body>
+</html>
