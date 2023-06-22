3b8a32e0fa8876ac071032ddcf2a507d9423968f
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:48:06 +0530

    Add footer page

---
+++ footer.php
@@ -0,0 +1,90 @@
+
+    <!-- Footer Start -->
+
+    <footer>
+        <div class="footerTopWrapper">
+            <div class="container">
+                <div class="innerWrap">
+                    <div class="footerColumn bgdark">
+                        <h2>Contact us</h2>
+                        <ul class="info">
+                            <li>
+                                <i class="las la-map-marker"></i>19, InfoCity 2, Hero Honda Chowk, Sector 33, Gurgaon, Haryana - 122001
+                            </li>
+                            <li>
+                                <i class="las la-phone"></i> <a href="tel:918860751566">+91-8860751566</a>
+                            </li>
+                            <li>
+                                <i class="las la-envelope"></i> <a href="mailto:info@superceuticals.in">info@superceuticals.in</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footerColumn bglight">
+                        <h2>Useful Link</h2>
+                        <ul class="list">
+                        <li>
+                                <a href="index.php">Home</a>
+                            </li>
+                            <li>
+                                <a href="about.php">About us</a>
+                            </li>
+                            <li>
+                                <a href="gallery.php">Gallery</a>
+                            </li>
+                            <!--<li>-->
+                            <!--    <a href="team.php">Our Team</a>-->
+                            <!--</li>-->
+                            <li>
+                                <a href="contact.php">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footerColumn bgdark">
+                        <h2>Our Location</h2>
+                        <div class="map">
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224090.76384509745!2d76.9531793867197!3d28.647194765412873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1639648983969!5m2!1sen!2sin"
+                                style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footerBottomWrapper">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-4 col-md-6">
+                        <a href="index.php" class="logo">
+                            <img src="src/img/white-logo.png" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <p class="m-0 center"><?php echo date('Y'); ?> &copy; Superceuticals Engineering Healthcare
+                        </p>
+                    </div>
+                    <div class="col-lg-4 col-md-12">
+                        <p class="m-0 right">Designed by <a href="https://www.1webexperts.com/" target="_blank"> 1Web Experts</a></p>
+                    </div>
+                </div>
+                <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-links">
+                        <ul style="list-style: none; padding: 0; margin: 0; display: flex; justify-content:center;">
+                            <li style="margin-right: 15px;"><a href="privacy-policy.php" style="color: #fff; text-decoration: none; font-size: 14px;">Privacy Policy</a></li>
+                            <li style="margin-right: 15px;"><a href="terms-conditions.php" style="color: #fff; text-decoration: none; font-size: 14px;">Terms & Conditions</a></li>
+                            <li style="margin-right: 15px;"><a href="refund-cancellation.php" style="color: #fff; text-decoration: none; font-size: 14px;">Refunds & Cancellation</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Footer End -->
+    <script src="src/js/jquery-3.6.0.min.js"></script>
+    <script src="src/js/bootstrap.bundle.min.js"></script>
+    <script src="src/js/owl.carousel.min.js"></script>
+    <script src="src/js/featherlight.gallery.js"></script>
+    <script src="src/js/featherlight.js"></script>
+    <script src="src/js/custom.js"></script>
