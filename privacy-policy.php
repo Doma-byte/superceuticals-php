8898ce97f1563bb44b9d00764c91e21f12b58c47
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:50:31 +0530

    Add privacy-policy page

---
+++ privacy-policy.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Superceuticals Engineering Healthcare</title>
+		<?php include 'headlink.php';?>
+	</head>
+	<body style="font-family: Arial,sans-serif; line-height: 1.6; margin: 0; padding:0;">
+		<!-- Header Area Start-->
+		<?php include 'header.php';?>
+		<!-- Header Area End -->
+		<div class="container" style="max-width: 800px; margin: 0 auto; padding: 20px;">
+        <h1 style="font-size: 26px; margin: 20px;">Superceuticals Private Limited</h1>
+        <h2 style="font-size: 24px; margin-bottom: 10px;">Privacy Policy</h2>
+        <p style="margin-bottom: 10px;">Last updated on Jun 19th 2023</p>
+
+        <p style="margin-bottom: 10px;">This privacy policy sets out how Superceuticals Private Limited uses and protects any information that you give Superceuticals Private Limited when you use this website.</p>
+
+        <p style="margin-bottom: 10px;">Superceuticals Private Limited is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, and then you can be assured that it will only be used in accordance with this privacy statement.</p>
+
+        <p style="margin-bottom: 10px;">Superceuticals Private Limited may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.</p>
+
+        <p style="margin-bottom: 10px;">We may collect the following information:</p>
+        <ul style="list-style-type: disc; margin-bottom: 10px; padding-left: 20px;">
+            <li>Name and job title</li>
+            <li>Contact information including email address</li>
+            <li>Demographic information such as postcode, preferences, and interests</li>
+            <li>Other information relevant to customer surveys and/or offers</li>
+        </ul>
+
+        <p style="margin-bottom: 10px;">What we do with the information we gather:</p>
+        <ul style="list-style-type: disc; margin-bottom: 10px; padding-left: 20px;">
+            <li>We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:</li>
+            <li>Internal record keeping.</li>
+            <li>We may use the information to improve our products and services.</li>
+            <li>We may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided.</li>
+            <li>From time to time, we may also use your information to contact you for market research purposes. We may contact you by email, phone, fax or mail. We may use the information to customize the website according to your interests.</li>
+        </ul>
+
+        <p style="margin-bottom: 10px;">We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure, we have put in suitable measures.</p>
+
+        <p style="margin-bottom: 10px;">How we use cookies:</p>
+        <p style="margin-bottom: 10px;">A cookie is a small file which asks permission to be placed on your computer's hard drive. Once you agree, the file is added and the cookie helps analyze web traffic or lets you know when you visit a particular site. Cookies allow web applications to respond to you as an individual. The web application can tailor its operations to your needs, likes, and dislikes by gathering and remembering information about your preferences.</p>
+
+        <p style="margin-bottom: 10px;">We use traffic log cookies to identify which pages are being used. This helps us analyze data about webpage traffic and improve our website in order to tailor it to customer needs. We only use this information for statistical analysis purposes and then the data is removed from the system.</p>
+
+        <p style="margin-bottom: 10px;">Overall, cookies help us provide you with a better website, by enabling us to monitor which pages you find useful and which you do not. A cookie in no way gives us access to your computer or any information about you, other than the data you choose to share with us.</p>
+
+        <p style="margin-bottom: 10px;">You can choose to accept or decline cookies. Most web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. This may prevent you from taking full advantage of the website.</p>
+
+        <p style="margin-bottom: 10px;">Controlling your personal information:</p>
+        <p style="margin-bottom: 10px;">You may choose to restrict the collection or use of your personal information in the following ways:</p>
+        <ul style="list-style-type: disc; margin-bottom: 10px; padding-left: 20px;">
+            <li>Whenever you are asked to fill in a form on the website, look for the box that you can click to indicate that you do not want the information to be used by anybody for direct marketing purposes.</li>
+            <li>If you have previously agreed to us using your personal information for direct marketing purposes, you may change your mind at any time by writing to or emailing us at singhdikshtit69@gmail.com.</li>
+        </ul>
+
+        <p style="margin-bottom: 10px;">We will not sell, distribute or lease your personal information to third parties unless we have your permission or are required by law to do so. We may use your personal information to send you promotional information about third parties which we think you may find interesting if you tell us that you wish this to happen.</p>
+
+        <p style="margin-bottom: 10px;">If you believe that any information we are holding on you is incorrect or incomplete, please write to or email us as soon as possible, at the above address. We will promptly correct any information found to be incorrect.</p>
+    </div>
+		<!-- Footer Area Start-->
+		<?php include 'footer.php';?>
+		<!-- Footer Area End-->
+	</body>
+</html>
