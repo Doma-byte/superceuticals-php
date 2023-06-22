7d1f73b9c1572299964af330a426f8ab851df631
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:47:17 +0530

    Add about page

---
+++ about.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>About Us | Superceuticals Engineering Healthcare</title>
+		<?php include 'headlink.php';?>
+	</head>
+	<body>
+		<!-- Header Area Start-->
+		<?php include 'header.php';?>
+		<!-- Header Area End -->
+		<section class="InnerBanner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<h1>About Us</h1>
+						<nav class="breadcrumb">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+								<li class="breadcrumb-item active" aria-current="page">About Us</li>
+							</ol>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="boxWrapper p-0 mt-5">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-6">
+						<div class="boxLeft">
+							<div class="missionWrapper">
+								<div class="content">
+									<p class="m-0">To Discover Develope And Deliver Innovative Healthcare Solutions For Patient-Centric Healthcare</p>
+									<h3 style="text-align:right; padding-right: 70px" class="mission">Mission</h3>
+								</div>
+								<div class="shapeImg">
+									<img src="src/img/shape/left-shape.png" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="boxRight">
+                        <div class="missionWrapper">
+								<div class="content">
+									<h3>Vision</h3>
+									<p>To Champion the Health and Well <br>Being of Every Citizen of India</p>
+								</div>
+								<div class="shapeRight">
+									<img src="src/img/shape/right-shape.png" alt="">
+								</div>
+							</div>
+                        </div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-6 order-lg-2">
+						<div class="contentTitle">
+							<!-- <p>About us</p> -->
+							<h2>About the Company</h2>
+						</div>
+						<p>Superceuticals began with a problem that everyone has faced - the tedious complexity of healthcare process of consulting, testing, report delivery and finally the prescription. We wanted to create an affordable, available and faster solution to the entire process.</p>
+						<p>Superceuticals Private Limited is a health-tech start-up recognized by DPIIT that develops and commercializes technological solutions in healthcare to bring ease to your lives. We offer solutions with diverse applications which simplify complexity, accelerate answers while delivering certainity.</p>
+						<p>We operate under strict regulations to bring to you the epitome of quality and dependence. Science drives us to create solutions that have the ability to bring a positive change and shape our society. Healthcare is a fundamental right but unfortunately is not as fundamentally available to all. We strive to achive our mission of patient centric healthcare.</p>
+						<p>We at Superceuticals, are committed to your health. Our team is continously engaged with exploring new clinical frontiers to bring early, faster and easier diagnosis.</p>
+					</div>
+					<div class="col-lg-6  order-lg-1">
+						<img src="src/img/about-thumb.jpg" class="w-100" alt="">
+					</div>
+				</div>
+				<!-- <div class="row">
+					<div class="col-lg-12 mt-4">
+					<p>Lorem ipsum dolor sit amet consectetur
+					        adipisicing elit. Sit repellat illum ipsa expedita nihil suscipit! Perspiciatis, quia.
+					        Autem, minima fugit aliquam deserunt magnam cupiditate. Vero, neque? Iusto incidunt
+					        voluptatem vitae? Lorem ipsum dolor sit amet consectetur
+					        adipisicing elit. Sit repellat illum ipsa expedita nihil suscipit! Perspiciatis, quia.
+					        Autem, minima fugit aliquam deserunt magnam cupiditate. Vero, neque? Iusto incidunt
+					        voluptatem vitae? Lorem ipsum dolor sit amet consectetur
+					        adipisicing elit. Sit repellat illum ipsa expedita nihil suscipit! Perspiciatis, quia.
+					        Autem, minima fugit aliquam deserunt magnam cupiditate. Vero, neque? Iusto incidunt
+					        voluptatem vitae?</p>
+					        <p>Lorem ipsum dolor sit amet consectetur
+					        adipisicing elit. Sit repellat illum ipsa expedita nihil suscipit! Perspiciatis, quia.
+					        Autem, minima fugit aliquam deserunt magnam cupiditate. Vero, neque? Iusto incidunt
+					        voluptatem vitae? Lorem ipsum dolor sit amet consectetur
+					        adipisicing elit. Sit repellat illum ipsa expedita nihil suscipit! Perspiciatis, quia.
+					        Autem, minima fugit aliquam deserunt magnam cupiditate. Vero, neque? Iusto incidunt
+					        voluptatem vitae?</p>
+					</div>
+					</div> -->
+			</div>
+		</section>
+		<!-- Footer Area Start-->
+		<?php include 'footer.php';?>
+		<!-- Footer Area End-->
+	</body>
+</html>
