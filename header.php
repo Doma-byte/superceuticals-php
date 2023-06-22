cce55a2983828c0f44e006b45972ab44016d2b64
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:48:30 +0530

    Add header page

---
+++ header.php
@@ -0,0 +1,131 @@
+<div class="preloader">
+    <div class="inner">
+        <div class="image">
+            <img src="src/img/logo.png" alt="">
+        </div>
+        <div class="content mt-4">
+            <h2>Healthcare Delivered Right</h2>
+        </div>
+        <div class="loadingImg">
+            <img src="src/img/icons/loader-2.gif" alt="">
+        </div>
+    </div>
+
+</div>
+<!-- Header Start -->
+<header>
+    <div class="headerTopBar pt-0 pb-0 pt-lg-3 pb-lg-3">
+        <div class="container">
+            <div class="logoBox">
+                <a href="index.php">
+                    <img src="src/img/logo.png" alt="logo">
+                </a>
+            </div>
+            <div class="rightWrap">
+                <div class="infoBox">
+                    <div class="column">
+                        <i class="las la-phone"></i>
+                        <div class="box">
+                            <p>Contact us</p>
+                            <a href="tel:918860751566">+91-8860751566</a>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <i class="las la-envelope"></i>
+                        <div class="box">
+                            <p>Mail us</p>
+                            <a href="mailto:info@superceuticals.in">info@superceuticals.in</a>
+                        </div>
+                    </div>
+                    <!-- <div class="column">
+                            <i class="las la-map-marker-alt"></i>
+                            <div class="box">
+                                <p>Our location</p>
+                                <span>Bangalore - 560016</span>
+                            </div>
+                        </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="headerBottomBar">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light p-0">
+                <a class="mobile-navbar-brand" href="index.php">
+                    <img src="src/img/logo.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#headerMenu" aria-controls="headerMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="headerMenu">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <!-- <li class="nav-item">
+                                <a class="nav-link" href="product.php">Product</a>
+                            </li> -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#">Resources<i class="las la-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="blog.php">Blogs</a>
+                                </li>
+                                <li>
+                                    <a href="manual.php">Manual</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#">About Us<i class="las la-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="about.php">Making a diffrence</a>
+                                </li>
+                                <li>
+                                    <a href="gallery.php">Gallery</a>
+                                </li>
+                                <!--<li>-->
+                                <!--    <a href="team.php">Our Team</a>-->
+                                <!--</li>-->
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact Us</a>
+                        </li>
+                    </ul>
+                    <ul class="socialLinks">
+                        <li>
+                            <a href="#"><i class="lab la-facebook-f"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="lab la-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="lab la-linkedin-in"></i></a>
+                        </li>
+                    </ul>
+
+                </div>
+                <li class="nav-item dropdown" style="margin-left: 50px; margin-top: 5px; padding: 5px;">
+                    <a class="nav-link" style= "color: red; text-decoration: none; font-size:large; font-weight: bold;" href="#">Login<i class="las la-angle-down" style="margin-left: 5px;"></i></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="http://app.superceuticals.in/auth/patient/login" target="_blank">Patient</a>
+                        </li>
+                        <li>
+                            <a href="http://app.superceuticals.in/auth/login" target="_blank">Technician</a>
+                        </li>
+                        <li>
+                            <a href="http://app.superceuticals.in/auth/pathlogist" target="_blank">Pathologist</a>
+                        </li>
+                        <li>
+                            <a href="http://app.superceuticals.in/auth/centre" target="_blank">Centre</a>
+                        </li>
+                    </ul>
+                    </li>
+            </nav>
+        </div>
+    </div>
+</header>
+<!-- Header End -->
