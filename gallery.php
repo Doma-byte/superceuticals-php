1a80e8f1b534101ee104f8a7fd7710d47db21a36
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:48:19 +0530

    Add gallery page

---
+++ gallery.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Gallery | Superceuticals Engineering Healthcare</title>
+		<?php include 'headlink.php';?>
+	</head>
+	<body>
+		<!-- Header Area Start-->
+		<?php include 'header.php';?>
+		<!-- Header Area End -->
+		<section class="InnerBanner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<h1>Gallery</h1>
+						<nav class="breadcrumb">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+								<li class="breadcrumb-item active" aria-current="page">Gallery</li>
+							</ol>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="galleryBox">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="galleryWrap">
+						    	<div class="item">
+								<div class="image">
+									<!--<a href="src/img/gallery/gallery3.jpg" class="thumbnail gallery">-->
+									<!--</a>-->
+									<img src="src/img/gallery/gallery3.jpg" alt="">
+								</div>
+								<div class="content">
+									<h3>PM Narendra Modi</h3>
+								</div>
+							</div>
+						<div class="item">
+								<div class="image">
+									<!--<a href="src/img/gallery/gallery4.jpg" class="thumbnail gallery">-->
+									<!--</a>-->
+									<img src="src/img/gallery/gallery4.jpg" alt="">
+								</div>
+								<div class="content">
+									<h3>Shri Munshuk Mandaviya, Central Health minister</h3>
+								</div>
+							</div>
+							
+							<div class="item">
+								<div class="image">
+									<!--<a href="src/img/gallery/gallery5.jpg" class="thumbnail gallery">-->
+									<!--</a>-->
+									<img src="src/img/gallery/gallery5.jpg" alt="">
+								</div>
+								<div class="content">
+									<h3>With Shri Pramod Kumar Mishra Principal secretary to the Prime Minister</h3>
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+									<!--<a href="src/img/gallery/gallery1.jpg" class="thumbnail gallery">-->
+									<!--</a>-->
+									<img src="src/img/gallery/gallery1.jpg" alt="">
+								</div>
+								<div class="content">
+									<h3>AK Tiwari, Member Technology, DOT</h3>
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+									<!--<a href="src/img/gallery/gallery2.jpg" class="thumbnail gallery">-->
+									<!--</a>-->
+										<img src="src/img/gallery/gallery2.jpg" alt="">
+								</div>
+								<div class="content">
+									<h3>Lt. General, Signals, Ministry of Defence</h3>
+								</div>
+							</div>
+							<!--<div class="item">-->
+							<!--	<div class="image">-->
+									<!--<a href="src/img/gallery/gallery6.jpg" class="thumbnail gallery">-->
+									<!--</a>-->
+							<!--		<img src="src/img/gallery/gallery6.jpg" alt="">-->
+							<!--	</div>-->
+							<!--	<div class="content">-->
+							<!--	<h3>Brig Bedi, Ministry of Defence</h3>-->
+							<!--	</div>-->
+							<!--</div>-->
+							
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery7.jpg" class="thumbnail gallery">-->
+								<!--</a>-->
+									<img src="src/img/gallery/gallery7.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery8.jpg" class="thumbnail gallery">-->
+								<!--</a>-->
+								<img src="src/img/gallery/gallery8.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery9.jpg" class="thumbnail gallery">-->
+								<!--</a>-->
+									<img src="src/img/gallery/gallery9.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery10.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery10.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery11.jpg" class="thumbnail gallery">-->
+								<!--</a>-->
+								<img src="src/img/gallery/gallery11.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery12.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery12.jpg" alt="">
+								</div>
+								
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery13.jpg" class="thumbnail gallery">-->
+								<!--</a>-->
+									<img src="src/img/gallery/gallery13.jpg" alt="">
+								</div>
+								
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery14.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery14.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery15.jpg" class="thumbnail gallery">-->
+								<!--</a>-->
+								<img src="src/img/gallery/gallery15.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery16.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery16.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery17.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery17.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery17.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery18.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery17.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery19.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery17.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery20.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery17.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery21.jpg" alt="">
+								</div>
+							</div>
+							<div class="item">
+								<div class="image">
+								<!--<a href="src/img/gallery/gallery17.jpg" class="thumbnail gallery">-->
+								
+								<!--</a>-->
+								<img src="src/img/gallery/gallery22.jpg" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Footer Area Start-->
+		<?php include 'footer.php';?>
+		<!-- Footer Area End-->
+
+		<script>		
+			$(document).ready(function(){
+				$('.gallery').featherlightGallery({
+					gallery: {
+						fadeIn: 300,
+						fadeOut: 300
+					},
+					openSpeed:    300,
+					closeSpeed:   300
+				});
+				$('.gallery2').featherlightGallery({
+					gallery: {
+						next: 'next »',
+						previous: '« previous'
+					},
+					variant: 'featherlight-gallery2'
+				});
+			});
+			
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//stats.g.doubleclick.net/dc.js','ga');
+			
+			ga('create', 'UA-5342062-6', 'noelboss.github.io');
+			ga('send', 'pageview');
+				
+		</script>
+		
+	</body>
+</html>
