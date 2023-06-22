2326c8f96b1b1948c20b161e3036bc1b5ca77d27
Dikshit <singhdikshit69@gmail.com>
2023-06-22 09:48:57 +0530

    Add navbar page

---
+++ headlink.php
@@ -0,0 +1,9 @@
+    <link rel="icon" href="src/img/logo-1.png" type="image/png">
+    <link rel="stylesheet" href="src/css/bootstrap.min.css">
+    <link rel="stylesheet" href="src/css/line-awesome.min.css">
+    <link rel="stylesheet" href="src/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="src/css/featherlight.css">
+    <link rel="stylesheet" href="src/css/featherlight.gallery.css">
+    <link rel="stylesheet" href="src/css/style.css">
+
+    
